666135650d11550865bbb73994dba482bba9560e
klaus <klauswang.2018@gmail.com>
2022-11-07 07:56:55 +0800

    fix: add notice-bar

--- pages/menu-list/menu-list.js
+++ pages/menu-list/menu-list.js
@@ -12,10 +12,17 @@ Page({
   async getMenuList() {
     const res = await getCategories()
     const tags = res.tags.map(tag => tag.name)
+    const list = []
 
-    const menuList = await Promise.all(tags.map(async tag => await getTopPlayList({ cat: tag })))
-    this.setData({
-      menuList
+    // 请求到一个分类数据就进行setData，以便于在页面进行展示
+    // 而不是等到所有的分类数据全部获取到后，统一进行setData
+    // 以避免页面出现长时间的白屏现象
+    tags.map(async (tag, index) => {
+      list[index] = await getTopPlayList({ cat: tag })
+      
+      this.setData({
+        menuList: list
+      })
     })
   }
 })
